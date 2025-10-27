d54904c7fa74201abb38c796d34aa9d23fec17cb
To Ngoc Hieu <hieudeptrai09@gmail.com>
2025-10-27 19:37:14 +0700

    change the home screen

--- fe/app/(home)/_components/Menu.jsx
+++ fe/app/(home)/_components/Menu.jsx
@@ -4,9 +4,9 @@ const Menu = ({ href, icon, label }) => {
   return (
     <Link
       href={href}
-      className="bg-yellow-300 rounded-2xl p-2 hover:bg-yellow-400 transform transition-all duration-300"
+      className="bg-purple-800 rounded-2xl p-2 hover:bg-purple-900 transform transition-all duration-300"
     >
-      <h2 className="text-2xl font-bold text-blue-600 text-center mb-2">
+      <h2 className="text-2xl font-bold text-white text-center mb-2">
         <span className="text-4xl">{icon}</span> {label}
       </h2>
     </Link>
