5b251751c987c33d98a5b524b24b18f059abcbca
To Ngoc Hieu <hieudeptrai09@gmail.com>
2025-11-01 00:19:16 +0700

    change the favicon and add some metadata file

--- fe/app/layout.tsx
+++ fe/app/layout.tsx
@@ -13,6 +13,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+          type="image/png"
+        />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body>{children}</body>
     </html>
   );
