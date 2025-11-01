2790b801f0a75177d0a0dedd2dc2b10328cebaa0
To Ngoc Hieu <hieudeptrai09@gmail.com>
2025-11-01 13:10:13 +0700

    fix bug cannot read value of undefined

--- fe/containers/fetcher.js
+++ fe/containers/fetcher.js
@@ -1,7 +1,7 @@
 const API_BASE =
-  import.meta.env.VITE_API_BASE ||
-  process.env.REACT_APP_API_BASE ||
-  process.env.NEXT_PUBLIC_API_BASE ||
+  import.meta?.env?.VITE_API_BASE ||
+  process?.env?.REACT_APP_API_BASE ||
+  process?.env?.NEXT_PUBLIC_API_BASE ||
   "http://typhoons-catfish.atwebpages.com/api.php";
 
 const fetchData = async (endpoint) => {
