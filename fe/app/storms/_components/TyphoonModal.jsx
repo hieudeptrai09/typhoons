f37f6c62159acd983148e1092d2c98b3b8408d72
hieudeptrai09 <hieudeptrai09@gmail.com>
2025-11-02 13:05:54 +0700

    change look and feel of the history list

--- fe/app/storms/_components/TyphoonModal.jsx
+++ fe/app/storms/_components/TyphoonModal.jsx
@@ -26,6 +26,23 @@ const TyphoonModal = ({ selectedCell, history, onClose }) => {
     }
   };
 
+  const getIntensityTextColor = (intensity) => {
+    switch (intensity) {
+      case "TD":
+      case "3":
+      case "4":
+      case "5":
+        return "white"; // White for dark backgrounds
+      case "TS":
+      case "STS":
+      case "1":
+      case "2":
+        return "gray"; // Black for light backgrounds
+      default:
+        return "white";
+    }
+  };
+
   return (
     <div
       className="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
@@ -46,16 +63,24 @@ const TyphoonModal = ({ selectedCell, history, onClose }) => {
         </div>
         <div className="flex gap-1.5 flex-col">
           {history.map((storm, index) => (
-            <div key={index} className="flex">
+            <div key={index} className="flex items-center">
               <span
-                className="text-white font-semibold w-7 h-7 flex items-center justify-center mr-1.5"
+                className="font-semibold w-10 h-10 flex items-center justify-center mr-1.5"
                 style={{
                   backgroundColor: getIntensityColor(storm.intensity),
+                  color: getIntensityTextColor(storm.intensity),
                 }}
               >
                 {storm.intensity}
               </span>
-              <span style={{ color: getIntensityColor(storm.intensity) }}>
+              <span
+                style={{
+                  color:
+                    storm.intensity === "1" || storm.intensity === "STS"
+                      ? "gray"
+                      : getIntensityColor(storm.intensity),
+                }}
+              >
                 {`${storm.name} (${storm.year})`}
               </span>
             </div>
