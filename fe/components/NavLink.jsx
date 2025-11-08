fac9088020e98211fda3395aabab724ac587a48c
To Ngoc Hieu <hieudeptrai09@gmail.com>
2025-11-08 11:29:03 +0700

    add text-white class

--- fe/components/NavLink.jsx
+++ fe/components/NavLink.jsx
@@ -4,7 +4,7 @@ const NavLink = ({ href, icon: Icon, label, isActive, onClick }) => {
   return (
     <Link
       href={href}
-      className={`flex items-center space-x-2 px-4 py-2 rounded-lg transition ${
+      className={`text-white flex items-center space-x-2 px-4 py-2 rounded-lg transition ${
         isActive ? "bg-white/30" : "hover:bg-white/20"
       }`}
       onClick={onClick}
