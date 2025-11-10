62a9fdaf684ac37d98707e930a52bbaabaac0cea
Hieu To <34306914+hieudeptrai09@users.noreply.github.com>
2025-11-10 09:59:05 +0700

    Change NavbarLogo text color to white

--- fe/components/NavBarLogo.jsx
+++ fe/components/NavBarLogo.jsx
@@ -5,7 +5,7 @@ const NavbarLogo = ({ onClose }) => {
   return (
     <Link
       href="/"
-      className="flex items-center space-x-2 hover:bg-white/20 px-4 py-2 rounded-lg transition"
+      className="text-white flex items-center space-x-2 hover:bg-white/20 px-4 py-2 rounded-lg transition"
       onClick={onClose}
     >
       <Home size={24} />
