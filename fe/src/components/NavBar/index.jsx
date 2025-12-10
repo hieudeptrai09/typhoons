c489f7e5cf874827de1d3f6e16b77c19ec6fd0de
To Ngoc Hieu <hieudeptrai09@gmail.com>
2025-12-11 00:53:11 +0700

    change the navbar logo to be more consistent

--- fe/src/components/NavBar/index.jsx
+++ fe/src/components/NavBar/index.jsx
@@ -2,10 +2,11 @@
 
 import { usePathname } from "next/navigation";
 import { useState } from "react";
-import NavbarLogo from "./NavBarLogo";
+import NavLink from "./NavLink";
 import DesktopNav from "./DesktopNav";
 import MobileNav from "./MobileNav";
 import MenuToggle from "./MenuToggle";
+import { Home } from "lucide-react";
 
 const Navbar = () => {
   const pathName = usePathname();
@@ -23,7 +24,12 @@ const Navbar = () => {
     <nav className="bg-blue-600 sticky top-0 z-10">
       <div className="max-w-7xl mx-auto px-2 py-2">
         <div className="flex items-center justify-between">
-          <NavbarLogo onClose={closeMenu} />
+          <NavLink
+            href="/"
+            icon={Home}
+            label="Home"
+            isActive={pathName === "/"}
+          />
           <DesktopNav currentPath={pathName} />
           <MenuToggle isOpen={isMenuOpen} onToggle={toggleMenu} />
         </div>
