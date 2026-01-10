ef0e96f464274daa3397e1fc93c142cacc099536
To Ngoc Hieu <hieudeptrai09@gmail.com>
2026-01-10 12:07:55 +0700

    redesing the card - option 1

--- fe/src/app/(navbar)/names/filter/_components/NameDetailsModal.tsx
+++ fe/src/app/(navbar)/names/filter/_components/NameDetailsModal.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { FileText, Globe, Languages } from "lucide-react";
 import Modal from "../../../../../components/Modal";
 import type { TyphoonName, BaseModalProps } from "../../../../../types";
 
@@ -24,50 +25,50 @@ const NameDetailsModal = ({ isOpen, onClose, selectedName }: NameDetailsModalPro
           : "!text-blue-600"
       }`}
     >
-      <div className="flex items-center gap-3">
-        <div className="flex-1 space-y-3">
-          <div>
-            <span className="font-semibold text-gray-700">Meaning:</span>
-            <span className="ml-2 text-gray-700">{selectedName.meaning}</span>
+      <div className="space-y-3">
+        <div className="mb-3 font-semibold text-indigo-700">{selectedName.meaning}</div>
+        <div className="flex items-start gap-3">
+          <div className="flex-1 space-y-3">
+            <div className="flex items-center gap-2">
+              <Globe className="h-4 w-4 text-emerald-700" />
+              <span className="text-gray-700">{selectedName.country}</span>
+            </div>
+
+            <div className="flex items-center gap-2">
+              <Languages className="h-4 w-4 text-emerald-700" />
+              <span className="text-gray-700">{selectedName.language}</span>
+            </div>
+
+            {!hasImage && hasDescription && (
+              <div className="flex items-start gap-2">
+                <FileText className="h-4 w-4 text-emerald-700" />
+                <p className="text-gray-700">{selectedName.description}</p>
+              </div>
+            )}
           </div>
 
-          <div>
-            <span className="font-semibold text-gray-700">Country:</span>
-            <span className="ml-2 text-gray-700">{selectedName.country}</span>
-          </div>
-
-          <div>
-            <span className="font-semibold text-gray-700">Language:</span>
-            <span className="ml-2 text-gray-700">{selectedName.language}</span>
-          </div>
-
-          {!selectedName.image && selectedName.description && (
-            <div>
-              <span className="font-semibold text-gray-700">Description:</span>
-              <span className="ml-2 text-gray-700">{selectedName.description}</span>
+          {selectedName.image && (
+            <div className="flex flex-1 flex-col gap-2">
+              <div
+                className="relative flex max-h-3/4 w-full items-start justify-center rounded-lg bg-gray-50"
+                style={{ aspectRatio: "4/3" }}
+              >
+                <Image
+                  src={selectedName.image}
+                  alt={selectedName.name}
+                  fill
+                  className="object-contain"
+                  unoptimized
+                />
+              </div>
+              {selectedName.description && (
+                <p className="text-center text-xs text-gray-700 italic">
+                  {selectedName.description}
+                </p>
+              )}
             </div>
           )}
         </div>
-
-        {selectedName.image && (
-          <div className={`flex flex-1 flex-col gap-2 ${!hasDescription && "self-end"}`}>
-            <div
-              className="relative flex max-h-3/4 justify-center rounded-lg bg-gray-50"
-              style={{ aspectRatio: "4/3" }}
-            >
-              <Image
-                src={selectedName.image}
-                alt={selectedName.name}
-                fill
-                className="object-contain"
-                unoptimized
-              />
-            </div>
-            {selectedName.description && (
-              <p className="text-center text-xs text-gray-700 italic">{selectedName.description}</p>
-            )}
-          </div>
-        )}
       </div>
     </Modal>
   );
