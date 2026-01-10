c70111ddda9217e2247920896db95c3200563a61
To Ngoc Hieu <hieudeptrai09@gmail.com>
2026-01-10 23:18:29 +0700

    fix bug

--- fe/src/components/Waiting/index.tsx
+++ fe/src/components/Waiting/index.tsx
@@ -1,7 +1,7 @@
 const Waiting = ({ content }: { content: string }) => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-stone-100">
-      <div className="text-xl text-red-600">{content}</div>
+      <div className="text-xl text-gray-700">{content}</div>
     </div>
   );
 };
