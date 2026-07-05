e85adc95e5772cbc2c79b8c776773196cf5cb010
To Ngoc Hieu <hieudeptrai09@gmail.com>
2026-07-06 00:47:32 +0700

    Revert "Revert "add intercepting modal""
    This reverts commit d1725ac8dd5cb6f98bd107ec0a46c8090c88858c.

---
+++ fe/src/app/@modal/(.)info/[name]/page.tsx
@@ -0,0 +1,18 @@
+import type { SearchDetail } from "@/lib/types";
+import { fetchServerData } from "@/lib/utils/fetchServerData";
+import InfoModal from "./InfoModal";
+
+interface InfoModalPageProps {
+  params: Promise<{ name: string }>;
+}
+
+export default async function InfoModalPage({ params }: InfoModalPageProps) {
+  const { name } = await params;
+  const decodedName = decodeURIComponent(name);
+
+  const result = await fetchServerData<SearchDetail>(
+    `/typhoon-names?name=${encodeURIComponent(decodedName)}`,
+  );
+
+  return <InfoModal detail={result?.data ?? null} name={decodedName} />;
+}
--- fe/src/lib/components/ImageWithLoader/index.tsx
+++ fe/src/lib/components/ImageWithLoader/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import Image from "next/image";
 import { useState } from "react";
 import type { ComponentProps } from "react";
-import Image from "next/image";
 import TyphoonSpinner from "../TyphoonSpinner";
 
 type ImageWithLoaderProps = ComponentProps<typeof Image>;
